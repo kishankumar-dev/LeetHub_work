2b3963f41f8c1f9ffeebd8df26151366a38fcde0
Kishan Kumar <120725655+kishan-k9@users.noreply.github.com>
2025-06-17 08:33:18 +0530

    Time: 3273 ms (46.88%) | Memory: 18 MB (84.38%) - LeetSync

---
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/count-the-number-of-arrays-with-k-matching-adjacent-elements.py
@@ -0,0 +1,5 @@
+class Solution:
+    def countGoodArrays(self, n: int, m: int, k: int) -> int:
+        mod = 10 ** 9 + 7
+        return m * pow(m - 1, n - k - 1, mod) * comb(n - 1, k) % mod
+        
