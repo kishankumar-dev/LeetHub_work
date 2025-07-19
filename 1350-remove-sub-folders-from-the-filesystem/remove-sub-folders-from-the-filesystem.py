95d11a5ddaa6802d69f55670423f1bd61f4f9d22
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2025-07-19 23:12:28 +0530

    Time: 22 ms (87.02%) | Memory: 30.6 MB (60.37%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,28 +1,10 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.is_end_of_word = False
- 
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        root = TrieNode()
-        for path in folder:
-            node = root
-            for ch in path.split('/'):
-                if ch not in node.children:
-                    node.children[ch] = TrieNode()
-                node = node.children[ch]
-            node.is_end_of_word = True
-
-
-        def bt(node, path):
-            if node.is_end_of_word:
-                return ['/'.join(path)]
-            ans = []
-            for ch in node.children:
-                path.append(ch)
-                ans += bt(node.children[ch], path)
-                path.pop()
-            return ans
-
-        return bt(root, [])
+        folder.sort()
+        ans = [folder[0]]
+        for i in range(1, len(folder)):
+            last_folder = ans[-1] + '/'
+            if not folder[i].startswith(last_folder):
+                ans.append(folder[i])
+        
+        return ans
