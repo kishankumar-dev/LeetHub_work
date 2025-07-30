09299ab7382e3b7926743ffe3ab4b1e034c2da52
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2025-07-30 07:14:20 +0530

    Time: 9 ms (100.00%) | Memory: 31.5 MB (5.92%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.py
@@ -1,19 +1,9 @@
-class Solution(object):
-    def longestSubarray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        length = 0
-        max_val = 0
-        for num in nums:
-            max_val = max(max_val, num)
-        count = 0
-        for num in nums:
-            if num == max_val:
-                count += 1
-                length = max(length, count)
-            else:
-                count = 0
-
-        return length
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        max_val = max(nums)
+        max_streak = max(
+            len(list(values))
+            for key, values in groupby(nums)
+            if key == max_val
+        )
+        return max_streak
