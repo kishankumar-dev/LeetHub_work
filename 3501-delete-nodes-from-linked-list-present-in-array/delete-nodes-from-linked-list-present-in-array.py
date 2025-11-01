bffbbf26f2a126b5b038c95be69b73d14a3d146c
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2025-11-01 14:50:22 +0530

    Time: 55 ms (74.24%) | Memory: 57.9 MB (97.55%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.py
@@ -1,15 +1,11 @@
-class Solution:
-    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head:
-            return None
-        num_set = set(nums)
-        dummy = ListNode(0)
-        dummy.next = head
-        prev, curr = dummy, head
-        while curr:
-            if curr.val in num_set:
-                prev.next = curr.next
-            else:
-                prev = curr
+class Solution(object):
+    def modifiedList(self, nums, head):
+        mpp = set(nums)
+        while head and head.val in mpp:
+            head = head.next
+        curr = head
+        while curr and curr.next:
+            while curr.next and curr.next.val in mpp:
+                curr.next = curr.next.next
             curr = curr.next
-        return dummy.next
+        return head
