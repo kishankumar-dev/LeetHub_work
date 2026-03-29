f05adf024058cda7ea15455d65eea8b82b816abf
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-03-29 17:56:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (15.79%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,19 +1,10 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        even1, odd1 = "", ""
-        even2, odd2 = "", ""
         
-        for i in range(len(s1)):
-            if i % 2 == 0:
-                even1 += s1[i]
-                even2 += s2[i]
-            else:
-                odd1 += s1[i]
-                odd2 += s2[i]
+        even_s1 = sorted([s1[0], s1[2]])
+        even_s2 = sorted([s2[0], s2[2]])
         
-        even1 = sorted(even1)
-        even2 = sorted(even2)
-        odd1 = sorted(odd1)
-        odd2 = sorted(odd2)
+        odd_s1 = sorted([s1[1], s1[3]])
+        odd_s2 = sorted([s2[1], s2[3]])
         
-        return even1 == even2 and odd1 == odd2
+        return even_s1 == even_s2 and odd_s1 == odd_s2
