4852ceb7d578bea4716d4b72e57bad034ab0f906
Kishan Kumar <120725655+kishankumar-dev@users.noreply.github.com>
2026-05-23 20:26:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (53.16%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,10 +1,3 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        n, rotations = len(nums), 0
-        for i in range(n):
-            if nums[i] > nums[(i + 1) % n]: # check adj numbers, % n to compare [last, first]
-                rotations += 1 # found a rotation
-                if rotations > 1: 
-                    return False # found more than 1 rotation
-        
-        return True # didn't find more than 1 rotation
+        return sum(nums[i - 1] > nums[i] for i in range(len(nums))) < 2
